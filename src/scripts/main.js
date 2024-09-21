f0a61d31c7510135199a368dc970c3ee895b17bd
Daniela Araujo <daniela.scaraujo@outlook.com>
2024-09-21 14:28:55 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const currentPosition = window.scrollY;
+
+        if (currentPosition < heroHeight) {
+            hideHeaderElements();
+        } else {
+            showHeaderElements();
+        }
+    })
+
+    //Seção de atraçõs, programação de abas
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const tabTarget = botao.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id=${tabTarget}]`)
+            hideAllTabs();
+            tab.classList.add('shows__list--is-active');
+            removeActiveButton();
+            botao.target.classList.add('shows__tabs__button--is-active');
+
+        })
+    }
+
+    //Seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', openOrCloseQuestion);
+    }
+})
+
+function hideHeaderElements () {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeaderElements () {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function openOrCloseQuestion(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    console.log(elemento);
+    const parentElement = elemento.target.parentNode
+
+    parentElement.classList.toggle(classe)
+}
+
+function removeActiveButton() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active')
+    }
+}
+
+function hideAllTabs () {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
